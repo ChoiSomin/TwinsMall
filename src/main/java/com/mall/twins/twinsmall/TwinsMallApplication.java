b5d534f5e6d62580fc3dcec4d381aafdf9194dec
pmr <milepark999@gmail.com>
2023-10-23 15:01:50 +0900

    commit test

--- src/main/java/com/mall/twins/twinsmall/TwinsMallApplication.java
+++ src/main/java/com/mall/twins/twinsmall/TwinsMallApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TwinsMallApplication {
+public class TwinsMallApplication { // test
 
     public static void main(String[] args) {
         SpringApplication.run(TwinsMallApplication.class, args);
