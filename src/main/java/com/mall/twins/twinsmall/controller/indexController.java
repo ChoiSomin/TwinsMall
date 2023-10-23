84eb0a07ffaf5e72e0941bee0b40f534596291fa
EunhaiKoh <yunhai0@naver.com>
2023-10-23 16:55:48 +0900

    index.html  구현함

--- src/main/java/com/mall/twins/twinsmall/controller/indexController.java
+++ src/main/java/com/mall/twins/twinsmall/controller/indexController.java
@@ -1,8 +1,18 @@
 package com.mall.twins.twinsmall.controller;
 
+import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
+@Log4j2
 public class indexController {
+
+    @GetMapping("/index")
+    public void list(){
+        log.info("list....");
+
+    }
+
 
 }
