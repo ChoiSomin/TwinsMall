4efff7ab640ef7255780f762628394e1d703a4ce
LeeJungmin <hiihjm104@gmail.com>
2023-10-23 14:56:14 +0900

    10/23 build.gradle, application setting done. ver 2.7.6

---
+++ src/main/java/com/mall/twins/twinsmall/TwinsMallApplication.java
@@ -0,0 +1,13 @@
+package com.mall.twins.twinsmall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwinsMallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TwinsMallApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mall/twins/twinsmall/TwinsMallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mall.twins.twinsmall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwinsMallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
