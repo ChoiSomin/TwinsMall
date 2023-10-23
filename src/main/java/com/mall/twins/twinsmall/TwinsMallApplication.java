35c9858e124d7db81176b54e0d63198ce0d5f65d
jjy <ioiuveon@gmail.com>
2023-10-23 15:06:58 +0900

    test

--- src/main/java/com/mall/twins/twinsmall/TwinsMallApplication.java
+++ src/main/java/com/mall/twins/twinsmall/TwinsMallApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TwinsMallApplication { // test
+public class TwinsMallApplication { // test123
 
     public static void main(String[] args) {
         SpringApplication.run(TwinsMallApplication.class, args);
