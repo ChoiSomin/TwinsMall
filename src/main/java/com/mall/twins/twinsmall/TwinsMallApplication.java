faaa11d8916ff6ec4bb59665c4f6f94c3f7e45df
schuetzj <haeun_thegrace@naver.com>
2023-10-24 11:29:55 +0900

    Entity 2차 최종본(DB테이블 모두 만들어짐) -Notice : pk 타입 String에서 Long으로 변경 -Order : 테이블명 orders로 변경 -Review : ToString 어노테이션에서 orders로 변경 -properties파일 : 하이버네이트 ddl 관련 설정 create에서 update로 변경 -TwinsMall Application : @EnableJpaAuditing 추가함

--- src/main/java/com/mall/twins/twinsmall/TwinsMallApplication.java
+++ src/main/java/com/mall/twins/twinsmall/TwinsMallApplication.java
@@ -2,8 +2,10 @@ package com.mall.twins.twinsmall;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class TwinsMallApplication {
 
     public static void main(String[] args) {
