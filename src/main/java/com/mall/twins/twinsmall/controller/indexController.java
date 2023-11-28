46285058ba930d6c62901e0434c60216173b12b1
parkm <milepark999@gmail.com>
2023-11-28 12:14:32 +0900

    접근 권한 핸들러 설정 (403에러 발생 시 얼럿창 띄우고 index페이지로 이동)

--- src/main/java/com/mall/twins/twinsmall/controller/indexController.java
+++ src/main/java/com/mall/twins/twinsmall/controller/indexController.java
@@ -9,9 +9,12 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Optional;
 
@@ -26,6 +29,11 @@ public class indexController {
     public String list() {
         log.info("list....");
         return "index";
+    }
+
+    @GetMapping("/auth/accessDenied")
+    public String accessDenied(){
+        return "accessDenied";
     }
 
     /*@GetMapping("/checkout")
